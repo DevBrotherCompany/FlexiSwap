292b1c926360d7694d68c7411cb04f5fdc67e813
Yurii Kabannik <yura_kabannik@icloud.com>
2022-09-07 21:10:46 +0300

    feat: Install hardhat

---
+++ contracts/README.md
@@ -0,0 +1,19 @@
+## Local development
+
+Install dependencies
+
+`npm ci`
+
+Copy enviroment variables
+
+`cp .env.example .env`
+
+## CLI scripts
+
+```shell
+npx hardhat help
+npx hardhat test
+GAS_REPORT=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.ts --network <NETWORK>
+```
---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.15',
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL ?? '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon: {
+      url: process.env.POLYGON_URL ?? '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+};
+
+export default config;
