c54954f8cefa115173f2c32daf20897f36a2b588
Yurii Kabannik <yura_kabannik@icloud.com>
2022-09-07 21:25:18 +0300

    feat: Add etherscan config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -19,6 +19,9 @@ const config: HardhatUserConfig = {
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY ?? '',
+  },
 };
 
 export default config;
