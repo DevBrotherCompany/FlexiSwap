61ff12eefd84c46ebab854d88f4013b22b46944b
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-09-10 20:12:12 +0300

    added slim prop

---
+++ web/src/components/FlexiButton/FlexiButton.styles.ts
@@ -0,0 +1,8 @@
+import { makeStyles } from "@mui/styles";
+
+export const useFlexiButtonStyles = makeStyles(() => ({
+  slim: {
+    paddingTop: "0 !important",
+    paddingBottom: "0 !important",
+  },
+}));
--- web/src/components/FlexiButton/FlexiButton.tsx
+++ web/src/components/FlexiButton/FlexiButton.tsx
@@ -1,5 +1,7 @@
 import React, { FC, ReactNode } from "react";
+import { useFlexiButtonStyles } from "./FlexiButton.styles";
 
+import cn from "classnames";
 import { ButtonProps } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
 
@@ -7,15 +9,23 @@ export interface FlexiButtonProps extends ButtonProps {
   loading?: boolean;
   loadingPosition?: "start" | "end";
   loadingIndicator?: ReactNode;
+  slim?: boolean;
 }
 
 export const FlexiButton: FC<FlexiButtonProps> = ({
   children,
   variant = "contained",
+  slim,
+  className,
   ...props
 }) => {
+  const classes = useFlexiButtonStyles();
   return (
-    <LoadingButton {...props} variant={variant}>
+    <LoadingButton
+      {...props}
+      className={cn(className, { [classes.slim]: slim })}
+      variant={variant}
+    >
       {children}
     </LoadingButton>
   );
