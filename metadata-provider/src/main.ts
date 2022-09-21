13dc4b48373652c16716833d5dae348eef72e61d
Yurii Kabannik <yura_kabannik@icloud.com>
2022-09-21 18:23:16 +0300

    fix: Add ports

--- metadata-provider/src/main.ts
+++ metadata-provider/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('PORT');
+  await app.listen(port);
 }
 bootstrap();
