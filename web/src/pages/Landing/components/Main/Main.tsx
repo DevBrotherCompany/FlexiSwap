1b39938f92fa1aa352556b486fa3fe8840a6ac6b
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-09-24 01:45:21 +0300

    added main landing image

--- web/src/pages/Landing/components/Main/Main.tsx
+++ web/src/pages/Landing/components/Main/Main.tsx
@@ -1,7 +1,10 @@
-import { FlexiButton } from 'components/FlexiButton/FlexiButton'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+
 import { RouteName } from 'shared/routes'
+import { FlexiButton } from 'components/FlexiButton/FlexiButton'
+
+import { ReactComponent as MainLanding } from 'pages/Landing/assets/mainImage.svg'
 
 export const Main: React.FC = () => {
   const navigate = useNavigate()
@@ -12,6 +15,7 @@ export const Main: React.FC = () => {
 
   return (
     <>
+      <MainLanding />
       <div style={{ width: 'fit-content' }}>
         <FlexiButton onClick={handleStartTrading} outlineColor='white' variant='outlined'>
           Start Trading
