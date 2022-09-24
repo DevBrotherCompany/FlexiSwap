bb0e4d8ce1fc09fd298e87f7a2ab07fd019a810d
Yurii Kabannik <yura_kabannik@icloud.com>
2022-09-24 03:03:14 +0300

    fix: Metamask auth

--- web/src/hooks/useAuth.ts
+++ web/src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
-import { useMoralis } from "react-moralis";
+import { useMoralis } from 'react-moralis';
 
 export const useAuth = () => {
-  const { user, isAuthenticated, authenticate, logout, account } = useMoralis();
-  return { user, isAuthenticated, login: authenticate, logout, account };
+  const { user, authenticate, logout, account, web3 } = useMoralis();
+  const signer = account && web3 ? web3.getSigner(account) : null;
+  return { user, login: authenticate, logout, account, signer };
 };
