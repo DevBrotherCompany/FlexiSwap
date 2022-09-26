e7ab52281ff91aa0100e323f88eb4951dbbc9239
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-09-26 19:00:03 +0300

    updated

--- web/src/pages/Trades/components/TradeList/TradeList.style.ts
+++ web/src/pages/Trades/components/TradeList/TradeList.style.ts
@@ -1,24 +1,26 @@
-import { makeStyles } from "@mui/styles";
-import { Theme } from "@mui/material/styles";
+import { makeStyles } from '@mui/styles'
+import { Theme } from '@mui/material/styles'
 
 export const useTradeListStyles = makeStyles((theme: Theme) => ({
-  list: { paddingTop: "60px" },
-  card: { paddingLeft: "55px !important" },
+  list: { paddingTop: '60px' },
+  card: { paddingLeft: '55px !important' },
   listItem: {
-    display: "grid",
-    gridTemplateColumns: "minmax(540px, 4fr) 1fr 4fr",
-    marginBottom: "20px !important",
+    display: 'grid',
+    gridTemplateColumns: 'minmax(540px, 4fr) 1fr 4fr',
+    marginBottom: '20px !important',
   },
   accordion: {
     backgroundColor: `${theme.palette.background.paper} !important`,
-    backgroundImage: "none !important",
-    cursor: "default !important",
-    padding: "10px 20px !important",
+    backgroundImage: 'none !important',
+    cursor: 'default !important',
+    padding: '10px 20px !important',
+    marginBottom: '50px !important',
+
     // flexDirection: "column",
-    "& .MuiAccordionSummary-content": {
-      display: "block !important",
+    '& .MuiAccordionSummary-content': {
+      display: 'block !important',
     },
   },
   // givings: { maxWidth: "520px" },
   givings: {},
-}));
+}))
