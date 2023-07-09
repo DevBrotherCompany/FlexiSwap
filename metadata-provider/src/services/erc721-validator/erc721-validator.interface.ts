06a4aed817f4f5486ce9285911b6dcad0f39e747
Yurii Kabannik <yura_kabannik@icloud.com>
2023-07-09 22:59:31 +0300

    chore: Remove unused code

--- metadata-provider/src/services/erc721-validator/erc721-validator.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface IERC721Validator {
-  isERC721<T extends { tokenAddress: string }>(
-    input: T,
-  ): Promise<T & { isERC721: boolean }>;
-
-  isCollectionERC721(collectionAddress: string): Promise<boolean>;
-}
--- metadata-provider/src/utils/is-address.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from 'ethers';
-
-export const isAddress = (input: string): boolean => {
-  return ethers.utils.isAddress(input);
-};
