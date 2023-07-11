d0bda47794e3b387d2734ae1da460c8e8d13ed17
Keretsman Illia <ilyaker1806@gmail.com>
2023-07-11 12:57:28 +0300

    Added toastify notifications

--- web2/hooks/useFlexiSwap.ts
+++ web2/hooks/useFlexiSwap.ts
@@ -4,8 +4,10 @@ import { FlexiSwap } from "@/sdk/flexi-swap";
 import { FlexiSwapAdapter } from "@/sdk/contracts/flexi-swap-adapter";
 import { FLEXISWAP_ADDRESS } from "@/sdk/constants";
 import { Approver } from "@/sdk/approver";
+import { IFlexiSwap } from "@/sdk/IFlexiSwap";
+import { FlexiSwapWithNotifications } from "@/sdk/flexi-swap-notifications";
 
-const useFlexiSwap = () => {
+const useFlexiSwap = (withNotifications = true) => {
   const { isConnected } = useAuth();
   const flexiSwapAddress: Address = useMemo(() => FLEXISWAP_ADDRESS, []);
 
@@ -14,8 +16,11 @@ const useFlexiSwap = () => {
 
     const flexiSwapAdapter = new FlexiSwapAdapter(flexiSwapAddress);
     const approver = new Approver(flexiSwapAddress);
-    return new FlexiSwap(flexiSwapAdapter, approver);
-  }, [isConnected, flexiSwapAddress]);
+    let flexiSwap: IFlexiSwap = new FlexiSwap(flexiSwapAdapter, approver);
+    if (withNotifications)
+      flexiSwap = new FlexiSwapWithNotifications(flexiSwap);
+    return flexiSwap;
+  }, [isConnected, flexiSwapAddress, withNotifications]);
 
   return flexiSwap;
 };
---
+++ web2/shared/ToastProvider/ToastProvider.tsx
@@ -0,0 +1,24 @@
+import { ToastContainer, TypeOptions } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      {children}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+};
+
+export default ToastProvider;
